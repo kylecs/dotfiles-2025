b80f08dc9129da080c0e3df41df0a1650d504f22
Kyle Spencer <kylecspencer@gmail.com>
2025-11-12 21:39:29 -0500

    lsp keybindings

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,4 +7,11 @@ return {
 			"nushell",
 		})
 	end,
+	keys = {
+		{ "K", function() return vim.lsp.buf.hover() end, desc = "Hover" },
+		{ "gd", vim.lsp.buf.definition, desc = "Goto Definition"},
+		{ "gr", vim.lsp.buf.references, desc = "References", nowait = true },
+		{ "<leader>cr", vim.lsp.buf.rename, desc = "Rename"},
+		{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "x" } },
+	},
 }
